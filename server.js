fd3584c14f1cb10f57c71ecc889589ba0b3d19f4
Anagha Rupesh <69381408+anagharm@users.noreply.github.com>
2021-05-10 09:50:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app'); // app file include
 const globalVariable = require("./nodemon.js");
-const port  = globalVariable.port;
+const port  = 3001;
 console.log('port ',port);
 
 const server = http.createServer(app);
